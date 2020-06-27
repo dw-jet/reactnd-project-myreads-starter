47d1e580141dd6888852020e7a41376dcc1c0333
James Teitsworth <james@jamesteitsworth.com>
2020-06-27 16:43:15 -0500

    refactor: Get library working with centralized state

--- src/BookSearch.js
+++ src/BookSearch.js
@@ -1,33 +1,18 @@
 import React, {Component} from 'react'
 import { Link } from 'react-router-dom'
 import Shelf from './Shelf'
-import * as BooksAPI from './BooksAPI'
 
 class BookSearch extends Component {
-  state = {
-    books: []
-  }
-
-  searchBooks = (e) => {
-    if (e) {
-      BooksAPI.search(e.target.value)
-        .then((books) => {
-          this.setState(() => ({
-            books
-          }))
-        })
-    }
-  }
 
   render() {
     return(
       <div className="search-books">
-        <div className="search-books-bar">
-          <Link className="close-search" to='/' >Close</Link>
-          <div className="search-books-input-wrapper">
-            <input type="text" onChange={this.searchBooks} placeholder="Search by title or author"/>
-          </div>
-        </div>
+      <div className="search-books-bar">
+      <Link className="close-search" to='/' >Close</Link>
+      <div className="search-books-input-wrapper">
+      <input type="text" onChange={this.searchBooks} placeholder="Search by title or author"/>
+      </div>
+      </div>
       <Shelf books={this.state.books} shelfName={{sentence: 'Search'}}/>
       </div>
       )
--- src/Library.js
+++ src/Library.js
@@ -1,63 +1,18 @@
 import React, { Component } from 'react'
 import Shelf from './Shelf'
-import * as BooksAPI from './BooksAPI'
 
 class Library extends Component {
-  state = {
-    books: [],
-
-    shelves: [
-      {
-        sentence: 'Currently Reading', 
-        code:'currentlyReading'
-      }, 
-      {
-        sentence: 'Want to Read',
-        code: 'wantToRead'
-      }, 
-      {
-        sentence: 'Read',
-        code: 'read'
-      }
-    ]
-  }
-
-  updateShelf = (book, newShelf) => {
-    BooksAPI.update(book, newShelf)
-      .then(() => {
-        this.loadBooks()
-      })
-  }
-
-  loadBooks() {
-    BooksAPI.getAll()
-      .then((books) => {
-        this.setState(() => ({
-          books
-        }))
-      })
-  }
-
-  componentDidMount() {
-    this.loadBooks()
-  }
-
-  filterShelf(shelfName) {
-    const books = this.state.books
+  render() {
     return(
-      books.filter((book)=>{
-        return book.shelf === shelfName.code;
+      this.props.shelves.map((shelf) => {
+        return <Shelf 
+        key={shelf.code} 
+        books={this.props.filterShelf(this.props.books, shelf)} 
+        shelfName={shelf} 
+        update={this.props.updateShelf} />
       })
-    )
+    )  
   }
-
-    render() {
-      return(
-        this.state.shelves.map((shelf) => {
-          return <Shelf key={shelf.code} books={this.filterShelf(shelf)} shelfName={shelf} update = {this.updateShelf} />
-        })
-      )  
-    }
-  }
-
+}
+  
   export default Library
